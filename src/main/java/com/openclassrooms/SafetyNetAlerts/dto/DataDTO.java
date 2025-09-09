eff4ec180c5f9796985f591cdff9c4827da1e7b6
Marine Tissier <marine.tsr13@icloud.com>
2025-09-09 09:42:46 +0200

    Fix duplication in data.json (persons/listPersons)

--- src/main/java/com/openclassrooms/SafetyNetAlerts/dto/DataDTO.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/dto/DataDTO.java
@@ -2,6 +2,7 @@ package com.openclassrooms.SafetyNetAlerts.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.openclassrooms.SafetyNetAlerts.model.FireStation;
 import com.openclassrooms.SafetyNetAlerts.model.MedicalRecord;
 import com.openclassrooms.SafetyNetAlerts.model.Person;
@@ -19,14 +20,17 @@ public class DataDTO {
 	private List<FireStation> firestations;
 	private List<MedicalRecord> medicalrecords;
 
+    @JsonIgnore
 	public List<Person> getListPersons() {
 		return persons;
 	}
 
+    @JsonIgnore
 	public List<MedicalRecord> getListMedicalRecords() {
 		return medicalrecords;
 	}
 
+    @JsonIgnore
 	public List<FireStation> getListFireStations() {
 		return firestations;
 	}
